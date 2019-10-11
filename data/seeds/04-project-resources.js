bd4054f19e26d10f69ac1d781c80616a033af949
Jonathan Ho <johnnyho82@gmail.com>
2019-10-11 12:14:51 -0400

    Add data seeding script 04-project-resources

--- data/seeds/04-project-resources.js
+++ data/seeds/04-project-resources.js
@@ -1,11 +1,9 @@
 exports.seed = function(knex) {
   return knex('project-resources').insert([
-    {
-      name: 'Project 1',
-      description: 'Description for Project 1',
-      completed: 0,
-    },
-    { id: 2, colName: 'rowValue2' },
-    { id: 3, colName: 'rowValue3' },
+    { project_id: 1, resource_id: 1 },
+    { project_id: 1, resource_id: 3 },
+    { project_id: 2, resource_id: 1 },
+    { project_id: 2, resource_id: 2 },
+    { project_id: 2, resource_id: 4 },
   ]);
 };
