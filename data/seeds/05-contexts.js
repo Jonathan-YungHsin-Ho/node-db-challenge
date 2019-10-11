f984a075fca23859f7e192ca8478889f10982657
Jonathan Ho <johnnyho82@gmail.com>
2019-10-11 19:16:34 -0400

    Add data seeding script for contexts and task-contexts table

---
+++ data/seeds/05-contexts.js
@@ -0,0 +1,8 @@
+exports.seed = function(knex) {
+  return knex('contexts').insert([
+    { context: 'at home' },
+    { context: 'at work' },
+    { context: 'at computer' },
+    { context: 'online' },
+  ]);
+};
