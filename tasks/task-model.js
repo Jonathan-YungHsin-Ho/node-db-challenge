6cb688774fac3bf4ceb6b80128cbfe825d558a1d
Jonathan Ho <johnnyho82@gmail.com>
2019-10-11 19:37:40 -0400

    Add GET /api/tasks/:id endpoint that lists all context related to task when retrieving task by id (stretch)

--- tasks/task-model.js
+++ tasks/task-model.js
@@ -2,6 +2,8 @@ const db = require('../data/db-config');
 
 module.exports = {
   getTasks,
+  getTaskById,
+  getTaskContexts,
 };
 
 function getTasks() {
@@ -14,4 +16,26 @@ function getTasks() {
       'p.name as project name',
       'p.description as project description',
     );
+}
+
+function getTaskById(id) {
+  const promises = [
+    db('tasks')
+      .where({ id })
+      .first(),
+    getTaskContexts(id),
+  ];
+
+  return Promise.all(promises).then(results => {
+    const [task, contexts] = results;
+
+    return { ...task, contexts };
+  });
+}
+
+function getTaskContexts(task_id) {
+  return db('contexts as c')
+    .join('task-contexts as tc', 'tc.context_id', 'c.id')
+    .select('c.context')
+    .where({ task_id });
 }
